0dfeaf3cf55547fdf3264f725db08b785a3b906d
Darren Ferguson <df@darren-ferguson.com>
2019-09-06 15:33:13 +0100

    Add usync

---
+++ src/Moriyama.AuthorisedMedia.Web/Controllers/TestSurfaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Moriyama.AuthorisedMedia.Web.Controllers
+{
+    class TestSurfaceController : SurfaceController
+    {
+        public ActionResult Login()
+        {
+            Members.Login("darren@moriyama.co.uk", "abc123abc123");
+            return Redirect("/");
+        }
+    }
+}
